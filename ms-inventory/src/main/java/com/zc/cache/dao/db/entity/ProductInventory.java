2810038cb2fdcc4b3fa2172091aa28b32bb13a1e
zhongcheng <zhongcheng_m@yeah.net>
2017-12-24 17:59:39 +0800

    init

---
+++ ms-inventory/src/main/java/com/zc/cache/dao/db/entity/ProductInventory.java
@@ -0,0 +1,52 @@
+package com.zc.cache.dao.db.entity;
+
+import java.io.Serializable;
+
+import com.baomidou.mybatisplus.activerecord.Model;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author zhongc
+ * @since 2017-12-24
+ */
+public class ProductInventory extends Model<ProductInventory> {
+
+    private static final long serialVersionUID = 1L;
+
+	private Long productId;
+	private Long inventoryCnt;
+
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public Long getInventoryCnt() {
+		return inventoryCnt;
+	}
+
+	public void setInventoryCnt(Long inventoryCnt) {
+		this.inventoryCnt = inventoryCnt;
+	}
+
+	@Override
+	protected Serializable pkVal() {
+		return this.productId;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductInventory{" +
+			"productId=" + productId +
+			", inventoryCnt=" + inventoryCnt +
+			"}";
+	}
+}
---
+++ ms-inventory/src/main/java/com/zc/cache/dao/db/mapper/ProductInventoryMapper.java
@@ -0,0 +1,16 @@
+package com.zc.cache.dao.db.mapper;
+
+import com.zc.cache.dao.db.entity.ProductInventory;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zhongc
+ * @since 2017-12-24
+ */
+public interface ProductInventoryMapper extends BaseMapper<ProductInventory> {
+
+}
